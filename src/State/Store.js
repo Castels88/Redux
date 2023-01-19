27496e1e64ee8b7c1f0a48891c6ebc9bf63f18e0
elio <elio.castellini@expleo.it>
2023-01-19 18:01:20 +0100

    fine prima parte

--- src/State/Store.js
+++ src/State/Store.js
@@ -1,16 +1,28 @@
 //1)scriviamo legacy_createStore e direttamente ci importera lo Store da redux
 //2)all interno dello Store mettiamo il nostro Reducer
 //3)mettiamo lo store dentro export const che possiamo exportare dove vogliamo
+//10) per combinare piu reducer si usa il combineReducer
 
-import { legacy_createStore } from "redux";
+import { legacy_createStore, combineReducers } from "redux";
 import {
   counterReducer,
   decrementCounter,
   incrementCounter,
   ResetCounter,
 } from "./CounterState";
+import { userReducer } from "./UserState";
+//11)inseriamo il combine dentro una const cosi da port in caso exportare
+//il CombineReducer sara una function con dentro un oggetto che avrà delle key e value.
+//come key andiamo a dare il un nome del reducer creato che ce lo faccia riconoscere
+//come value mettiamo il nostro reducer
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  user: userReducer,
+});
 
-export const store = legacy_createStore(counterReducer);
+//12)e per finire possiamo inserire il combine reducer dentro lo store in modo da averli entrambi
+
+export const store = legacy_createStore(rootReducer);
 
 //4)adesso andiamo ad utilizzare lo store. lo store è un oggetto e ha diversi metodi
 //5)uno di questi metodi è il Dispatch  method che riceve un azione e la manda allo Store stesso
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import "./Style/index.css";
 import { Root } from "./Root";
+import { store } from "./State/Store";
+import { add } from "./State/UserState";
+import {
+  incrementCounter,
+  decrementCounter,
+  ResetCounter,
+} from "./State/CounterState";
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<Root />);
+const root = document.querySelector("#root");
+// root.render(<Root />);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(incrementCounter(2)); //altra cosa che posso fare è assegnare il valore della action
+store.dispatch(decrementCounter(5));
+store.dispatch(ResetCounter());
+store.dispatch(add({ id: 1, name: "Elio", age: "34" }));
